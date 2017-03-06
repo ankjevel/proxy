e273887bc7668123815dbf50ebafb8771bfc411e
Dennis Pettersson <mail@dennispettersson.se>
2017-03-06 22:53:36 +0100

    path replace

--- generate.sh
+++ generate.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 ENDPOINT=$([[ ! -z $ENDPOINT ]] && echo "$ENDPOINT" || echo "https://test.sh")
+ENDPOINT=$(echo $ENDPOINT | sed -e 's/\/+$//')
 
 cat <<EOF > /etc/nginx/conf.d/default.conf
 server {
   server_tokens off;
 
   location / {
-    proxy_pass $ENDPOINT;
-    proxy_http_version 1.1;
-    proxy_set_header Upgrade \$http_upgrade;
-    proxy_set_header Connection "upgrade";
+    rewrite /(.*)/\$ /\$1;
+    proxy_buffering off;
     proxy_set_header Host \$host;
+    proxy_set_header X-Real-IP \$remote_addr;
+    proxy_pass $ENDPOINT/;
   }
 }
 EOF
